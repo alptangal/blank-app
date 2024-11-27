b6395ed57dd49a5816ce633f523fe0098b6929bf
alptangal <alptangal@gmail.com>
2024-11-27 16:18:04 +0700

    .

---
+++ streamlit_app1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import subprocess
+
+subprocess.run([''])
+
+st.title("🎈 My new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
