71883f94eaa8ba76fc2445e8b860d56163b830c3
alptangal <alptangal@gmail.com>
2024-11-27 18:37:51 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import datetime
 import traceback
 
-try:
+'''try:
     req=requests.get('http://localhost:8000')
     print(req)
     if 'hello' not in req.text:
@@ -12,7 +12,7 @@ try:
     sys.exit("Exited")
 except Exception as error:
     traceback.print_exc()
-    subprocess.run(['uvicorn','app:app', '--reload'])
+    subprocess.run(['uvicorn','app:app', '--reload'])'''
 
 if not hasattr(st, 'already_started_server'):
     # Hack the fact that Python modules (like st) only load once to
