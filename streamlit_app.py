e8f7b5a415b4bab2158e8419c87f4cede51a2b1e
alptangal <alptangal@gmail.com>
2024-11-27 18:00:46 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import subprocess,sys
 import requests
 import datetime
+import traceback
 
 try:
     req=requests.get('http://localhost:8000')
@@ -9,10 +10,28 @@ try:
         raise Exception("Server not response")
     sys.exit("Exited")
 except Exception as error:
-    print(error)
+    traceback.print_exc()
     subprocess.run(['uvicorn','app:app', '--reload'])
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+if not hasattr(st, 'already_started_server'):
+    # Hack the fact that Python modules (like st) only load once to
+    # keep track of whether this file already ran.
+    st.already_started_server = True
+
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
+
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
+
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    @app.route('/foo')
+    def serve_foo():
+        return 'This page is served via Flask!'
+
+    app.run(port=8888)
